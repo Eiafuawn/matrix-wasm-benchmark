8ac13716ab0b0a074a0147086ae2c1687b11530f
Eiafuawn <kenan.henzelin@proton.me>
2024-08-28 15:40:01 +0300

    Better benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Define an array of matrix sizes to test
-sizes=(100 200 300 400 500)
+if [ "$#" -eq 0 ]; then
+    sizes=(100 200 300 400 500)
+else
+    sizes=("$@")
+    echo "Testing with custom sizes: ${sizes[@]}"
+fi 
 
 # Loop through each size and run the JavaScript and Rust benchmarks
 for size in "${sizes[@]}"; do
